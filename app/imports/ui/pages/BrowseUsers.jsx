42ff7f45f8d9a99ff9cc6d8b0b35bfed03385bf3
ethancheez <61472206+ethancheez@users.noreply.github.com>
2021-04-07 02:56:56 -1000

    update styles

--- app/imports/ui/pages/BrowseUsers.jsx
+++ app/imports/ui/pages/BrowseUsers.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Header, Card, Image, Label } from 'semantic-ui-react';
+import { Container, Header, Card, Image, Label, Input } from 'semantic-ui-react';
 import { withRouter, Link } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
@@ -8,6 +8,10 @@ class BrowseUsers extends React.Component {
     return (
       <div className='music-background'>
         <Container>
+          <Input fluid
+            icon={{ name: 'search', circular: true, link: true }}
+            placeholder='Search User...'
+          />
           <Header as="h2" textAlign="center" inverted>Browse Users</Header>
           <Card.Group centered>
             <Card>
@@ -22,20 +26,6 @@ class BrowseUsers extends React.Component {
               </Card.Content>
               <Card.Content extra>
                 <Link to="/viewprofile">View Profile</Link>
-              </Card.Content>
-            </Card>
-
-            <Card>
-              <Card.Content>
-                <Image floated='right' size='mini' src=''/>
-                <Card.Header>John Doe</Card.Header>
-                <Card.Description>Goals: </Card.Description>
-                <Header as='h4'>Music Interests:</Header>
-                <Label>stuff</Label>
-                <Label>other stuff</Label>
-              </Card.Content>
-              <Card.Content extra>
-                <Link to="/ViewProfile">View Profile</Link>
               </Card.Content>
             </Card>
 
