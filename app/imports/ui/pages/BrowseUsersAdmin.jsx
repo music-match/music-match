c0e983f96195f117973f9b77448bd0f8619db017
adamjparrilla <ajp808@hawaii.edu>
2021-04-07 20:15:19 -1000

    added browse users admin looks

--- app/imports/ui/pages/BrowseUsersAdmin.jsx
+++ app/imports/ui/pages/BrowseUsersAdmin.jsx
@@ -1,15 +1,118 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { Container, Header, Card, Image, Label, Input } from 'semantic-ui-react';
+import { withRouter, Link } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
 class BrowseUsersAdmin extends React.Component {
   render() {
     return (
-      <Header as="h2" textAlign="center" inverted>
-        <p>Implement Code Here</p>
-      </Header>
+      <div className='music-background'>
+        <Container>
+          <Input fluid
+            icon={{ name: 'search', circular: true, link: true }}
+            placeholder='Search User...'
+          />
+          <Header as="h2" textAlign="center" inverted>Browse Users (Admin)</Header>
+          <Card.Group centered>
+            <Card>
+              <Card.Content>
+                <Image floated='right' size='mini' src='https://ethancheez.github.io/images/ethan.jpg'/>
+                <Card.Header>Ethan Chee</Card.Header>
+                <Card.Description>Goals: Become a Computer Engineer, Develop Innovative Technologies</Card.Description>
+                <Header as='h4'>Music Interests:</Header>
+                <Label>Pop</Label>
+                <Label>Alternate</Label>
+              </Card.Content>
+              <Card.Content extra>
+                <Link to="/viewprofile">View Profile</Link>
+              </Card.Content>
+            </Card>
+
+            <Card>
+              <Card.Content>
+                <Image floated='right' size='mini' src=''/>
+                <Card.Header>John Doe</Card.Header>
+                <Card.Description>Goals: </Card.Description>
+                <Header as='h4'>Music Interests:</Header>
+                <Label>stuff</Label>
+                <Label>other stuff</Label>
+              </Card.Content>
+              <Card.Content extra>
+                <Link className='admin-link' to="/ViewProfile">View</Link>
+                <Link className='admin-link' to="/ViewProfile">Edit</Link>
+                <Link className='admin-link' to="/ViewProfile">Delete</Link>
+              </Card.Content>
+            </Card>
+
+            <Card>
+              <Card.Content>
+                <Image floated='right' size='mini' src=''/>
+                <Card.Header>John Doe</Card.Header>
+                <Card.Description>Goals: </Card.Description>
+                <Header as='h4'>Music Interests:</Header>
+                <Label>stuff</Label>
+                <Label>other stuff</Label>
+              </Card.Content>
+              <Card.Content extra>
+                <Link className='admin-link' to="/ViewProfile">View</Link>
+                <Link className='admin-link' to="/ViewProfile">Edit</Link>
+                <Link className='admin-link' to="/ViewProfile">Delete</Link>
+              </Card.Content>
+            </Card>
+
+            <Card>
+              <Card.Content>
+                <Image floated='right' size='mini' src=''/>
+                <Card.Header>John Doe</Card.Header>
+                <Card.Description>Goals: </Card.Description>
+                <Header as='h4'>Music Interests:</Header>
+                <Label>stuff</Label>
+                <Label>other stuff</Label>
+              </Card.Content>
+              <Card.Content extra>
+                <Link className='admin-link' to="/ViewProfile">View</Link>
+                <Link className='admin-link' to="/ViewProfile">Edit</Link>
+                <Link className='admin-link' to="/ViewProfile">Delete</Link>
+              </Card.Content>
+            </Card>
+
+            <Card>
+              <Card.Content>
+                <Image floated='right' size='mini' src=''/>
+                <Card.Header>John Doe</Card.Header>
+                <Card.Description>Goals: </Card.Description>
+                <Header as='h4'>Music Interests:</Header>
+                <Label>stuff</Label>
+                <Label>other stuff</Label>
+              </Card.Content>
+              <Card.Content extra>
+                <Link className='admin-link' to="/ViewProfile">View</Link>
+                <Link className='admin-link' to="/ViewProfile">Edit</Link>
+                <Link className='admin-link' to="/ViewProfile">Delete</Link>
+              </Card.Content>
+            </Card>
+
+            <Card>
+              <Card.Content>
+                <Image floated='right' size='mini' src=''/>
+                <Card.Header>John Doe</Card.Header>
+                <Card.Description>Goals: </Card.Description>
+                <Header as='h4'>Music Interests:</Header>
+                <Label>stuff</Label>
+                <Label>other stuff</Label>
+              </Card.Content>
+              <Card.Content extra>
+                <Link className='admin-link' to="/ViewProfile">View</Link>
+                <Link className='admin-link' to="/ViewProfile">Edit</Link>
+                <Link className='admin-link' to="/ViewProfile">Delete</Link>
+              </Card.Content>
+            </Card>
+          </Card.Group>
+        </Container>
+      </div>
+
     );
   }
 }
 
-export default BrowseUsersAdmin;
+export default withRouter(BrowseUsersAdmin);
