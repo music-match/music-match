a15ac63f1d5e02d6afc50ea8dfd50aa07033394b
adamjparrilla <ajp808@hawaii.edu>
2021-04-19 02:20:35 -1000

    issue-27

--- app/imports/ui/pages/BrowseJams.jsx
+++ app/imports/ui/pages/BrowseJams.jsx
@@ -6,6 +6,7 @@ import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Jams } from '../../api/profile/Jams';
 import JamCard from '../components/JamCard';
+import { Profiles } from '../../api/profile/Profiles';
 
 function alphaSort(jams) {
   return _.sortBy(jams, function (jam) { return jam.title.toLowerCase(); });
@@ -26,7 +27,10 @@ class BrowseJams extends React.Component {
         <Container>
           <Header inverted as="h2" textAlign="center">Browse Jams</Header>
           <Card.Group centered itemsPerRow={3}>
-            {alphaSort(this.props.jams).map((jam, index) => <JamCard key={index} jam={jam}/>)}
+            {alphaSort(this.props.jams).map((jam, index) => <JamCard
+              key={index}
+              jam={jam}
+              profile={(this.props.profiles).filter(profile => (jam.email === profile.email))}/>)}
           </Card.Group>
         </Container>
       </div>
@@ -37,6 +41,7 @@ class BrowseJams extends React.Component {
 // Require an array of Stuff documents in the props.
 BrowseJams.propTypes = {
   jams: PropTypes.array.isRequired,
+  profiles: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
@@ -44,12 +49,15 @@ BrowseJams.propTypes = {
 export default withTracker(() => {
   // Get access to Stuff documents.
   const subscription = Meteor.subscribe(Jams.userPublicationName);
+  const subscription2 = Meteor.subscribe(Profiles.userPublicationName);
   // Determine if the subscription is ready
-  const ready = subscription.ready();
+  const ready = subscription.ready() && subscription2.ready();
   // Get the Stuff documents
   const jams = Jams.collection.find({}).fetch();
+  const profiles = Profiles.collection.find({}).fetch();
   return {
     jams,
+    profiles,
     ready,
   };
 })(BrowseJams);
