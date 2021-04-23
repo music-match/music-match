243b89d4256112baa79771882557b8e552e3f0ab
ethancheez <61472206+ethancheez@users.noreply.github.com>
2021-04-22 14:39:51 -1000

    added condition if nothing shows up from search filter

--- app/imports/ui/pages/BrowseJamsAdmin.jsx
+++ app/imports/ui/pages/BrowseJamsAdmin.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { _ } from 'meteor/underscore';
-import { Container, Header, Loader, Card } from 'semantic-ui-react';
+import { Container, Header, Loader, Card, Input, Image } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import JamCardAdmin from '../components/JamCardAdmin';
@@ -16,8 +16,27 @@ function getProfile(profiles, jam) {
   return _.find(profiles, function (profile) { return jam.email === profile.email; });
 }
 
+function filterJams(jams, search) {
+  if (search === '') {
+    return jams;
+  }
+  const filteredJams = _.filter(jams, function (jam) { return jam.title.toLowerCase().indexOf(search) >= 0 || jam.description.toLowerCase().indexOf(search) >= 0; });
+  return filteredJams;
+}
+
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class BrowseJamsAdmin extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      searchField: '',
+    };
+  }
+
+  handleSearch(input) {
+    this.setState({ searchField: input.target.value });
+  }
 
   // If the subscription(s) have been received, render the page, otherwise show a loading icon.
   render() {
@@ -26,17 +45,34 @@ class BrowseJamsAdmin extends React.Component {
 
   // Render the page once subscriptions have been received.
   renderPage() {
+    const { searchField } = this.state;
     return (
       <div className='music-background'>
         <Container>
           <Header inverted as="h2" textAlign="center">Browse Jams (Admin)</Header>
+          <div className='search-padding'>
+            <Input fluid onChange={this.handleSearch.bind(this)}
+              placeholder='Search Jams by Name...'
+            />
+          </div>
           <Card.Group centered itemsPerRow={3}>
-            {alphaSort(this.props.jams).map((jam, index) => <JamCardAdmin
-              key={index}
-              jam={jam}
-              profile={getProfile(this.props.profiles, jam)}/>)}
+            {(_.size(filterJams(this.props.jams, searchField.toLowerCase())) > 0) ?
+              (alphaSort(filterJams(this.props.jams, searchField.toLowerCase())).map((jam, index) => <JamCardAdmin
+                key={index}
+                jam={jam}
+                profile={getProfile(this.props.profiles, jam)}/>)) : this.displayNoJams()
+            }
           </Card.Group>
         </Container>
+      </div>
+    );
+  }
+
+  displayNoJams() {
+    return (
+      <div style={{ paddingTop: '200px', paddingBottom: '200px' }}>
+        <Header inverted as='h2'>No Jams Found from Filter</Header>
+        <Image centered src='https://c.tenor.com/HJvqN2i4Zs4AAAAj/milk-and-mocha-cute.gif'/>
       </div>
     );
   }
