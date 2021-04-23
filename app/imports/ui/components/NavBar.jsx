72885e6397f22e8cbdd6a7cbae5dfdbe338eb6bd
Isaiah Eusebio <icce@hawaii.edu>
2021-04-22 16:22:32 -1000

    update navbar editprofile link

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -60,7 +60,7 @@ class NavBar extends React.Component {
             <Dropdown id="navbar-current-user" text={this.props.username} pointing="top right" icon={'user'}>
               <Dropdown.Menu>
                 {this.props.profile ? (
-                  <Dropdown.Item id="navbar-edit-profile" icon="edit" text="Edit Profile" as={NavLink} exact to="/editprofile"/>
+                  <Dropdown.Item id="navbar-edit-profile" icon="edit" text="Edit Profile" as={NavLink} exact to={`/editprofile/${this.props.profile._id}`}/>
                 ) : ''}
                 <Dropdown.Item id="navbar-sign-out" icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
               </Dropdown.Menu>
